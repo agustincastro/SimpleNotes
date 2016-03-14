e974d97311a50bc1ed7f8746f98ae37106884755
Agustin Castro <agustin.castro.91@gmail.com>
2016-03-13 22:35:02 -0300

    Crome storage test and better button style

--- simplenotes.js
+++ simplenotes.js
@@ -20,3 +20,28 @@ function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 };
+
+
+
+
+// CHROME STORAGE
+// refer to https://developer.chrome.com/extensions/storage
+
+function saveChanges() {
+        // Get a value saved in a form.
+        var theValue = textarea.value;
+        // Check that there's some code there.
+        if (!theValue) {
+          message('Error: No value specified');
+          return;
+        }
+        // Save it using the Chrome extension storage API.
+        chrome.storage.sync.set({'value': theValue}, function() {
+          // Notify that we saved.
+          message('Settings saved');
+        });
+      };
+
+
+
+
