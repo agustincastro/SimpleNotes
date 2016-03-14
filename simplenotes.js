f201cce413b3fc75e7bac545227c759c75a95cb3
Agustin Castro <agustin.castro.91@gmail.com>
2016-03-14 00:08:48 -0300

    Delete note icon and js interaction

--- simplenotes.js
+++ simplenotes.js
@@ -1,24 +1,37 @@
 
+//Jquery DOM variables for commonly used nodes, more performant
 var $newNoteButton = $("#new-note");
 var $noteTemplate = $("#note-template");
 var $notesContainer = $("#notes-container");
-
+//var $noteDeleteButton = $("#delete-note");
 
 
 //Create new note
 $newNoteButton.click(function(event){
-  var noteTemplate = $noteTemplate.clone();
-  noteTemplate.removeClass("invisible");
-  $notesContainer.append(noteTemplate);
-  console.log("golazo!");
+	var noteTemplate = $noteTemplate.clone();
+	noteTemplate.removeClass("invisible");
+	noteTemplate.find("#delete-note").on("click", DeleteNote);
+	$notesContainer.append(noteTemplate);
+
+  //TODO: Guardar la nota en storage  
 });
 
+
+//Delete note
+function DeleteNote(event){
+	var html = $(event.target);
+	html.parent().remove();
+	
+	//$notesContainer.
+	//TODO: Borrar la nota del storage y dsps hace el 
+	//borrado de html
+};
 
 
 //HELPERS
 function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
 };
 
 
@@ -32,15 +45,15 @@ function saveChanges() {
         var theValue = textarea.value;
         // Check that there's some code there.
         if (!theValue) {
-          message('Error: No value specified');
-          return;
+        	message('Error: No value specified');
+        	return;
         }
         // Save it using the Chrome extension storage API.
         chrome.storage.sync.set({'value': theValue}, function() {
           // Notify that we saved.
           message('Settings saved');
-        });
-      };
+      });
+    };
 
 
 
